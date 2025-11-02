66491627058b69fd04ff0ac6341667f53694b2ef
AlexeyOn <xalexvp@gmail.com>
2025-11-03 01:33:42 +0200

    map ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef } from 'react';
+import mapboxgl from 'mapbox-gl';
+import mapboxglCompare from 'mapbox-gl-compare';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import 'mapbox-gl/dist/mapbox-gl.css';
+import 'mapbox-gl-compare/dist/mapbox-gl-compare.css';
+
+// extend mapboxgl, adding the Compare class
+mapboxgl.Compare = mapboxglCompare;
+
+const MapboxExample = () => {
+    const mapRef = useRef();
+    const beforeMapContainerRef = useRef();
+    const afterMapContainerRef = useRef();
+    const comparisonContainerRef = useRef();
+
+    const mapStyle = { position: 'absolute', top: 0, bottom: 0, width: '100%' };
+
+    useEffect(() => {
+        // some development servers will run this hook more than once
+        // return if the map has already been initialized
+        if (mapRef.current) return;
+
+        mapboxgl.accessToken = process.env.REACT_APP_MAP_TOKEN;
+
+        const beforeMap = new mapboxgl.Map({
+            container: beforeMapContainerRef.current,
+            style: 'mapbox://styles/mapbox/standard',
+            config: {
+                basemap: {
+                    theme: 'monochrome'
+                }
+            },
+            center: [0, 0],
+            zoom: 5
+        });
+
+        const afterMap = new mapboxgl.Map({
+            container: afterMapContainerRef.current,
+            style: 'mapbox://styles/mapbox/standard',
+            config: {
+                basemap: {
+                    theme: 'monochrome',
+                    lightPreset: 'night'
+                }
+            },
+            center: [0, 0],
+            zoom: 5
+        });
+
+        mapRef.current = new mapboxgl.Compare(
+            beforeMap,
+            afterMap,
+            comparisonContainerRef.current
+        );
+    }, []);
+
+    return (
+        <div
+            id="comparison-container"
+            ref={comparisonContainerRef}
+            style={{ height: '98vh', position: 'relative' }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+            <div id="before" ref={beforeMapContainerRef} style={mapStyle}></div>
+            <div id="after" ref={afterMapContainerRef} style={mapStyle}></div>
+        </div>
+    );
+};
 
-export default App;
+export default MapboxExample;
